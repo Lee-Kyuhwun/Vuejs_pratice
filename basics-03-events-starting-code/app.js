44dec6f321d90030e37994dabb8b5fe21df8b836
steveLee <steve.lee@obigo.com>
2023-12-07 14:53:17 +0900

    두번째 과제 전 이멘트 관련 실습

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -1,9 +1,37 @@
+
+
+
 const app = Vue.createApp({
   data() {
     return {
-      counter: 0,
+      counter: 10,
+      name: '',
+      confirmedName: ''
     };
   },
+  methods:{
+    confirmInput(){
+      this.confirmedName = this.name;
+    },
+
+    submitForm(event){
+      // event.preventDefault(); // prevent the default action of the form 
+      alert('Submitted!');
+    },
+    setName(event){
+      this.name = event.target.value;
+    },
+
+    add(num){
+      this.counter = this.counter + num;
+    },
+    reduce(num){
+      this.counter = this.counter - num;
+    },
+    reset(){
+      this.counter = 0;
+    }
+  }
 });
 
 app.mount('#events');
